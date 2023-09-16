b12dbe3cbebcedafcbb52e05d9e71ae9cec516c3
nisann97 <nisann@code.edu.az>
2023-09-16 04:57:10 +0400

    Added homework

---
+++ Homework/Program.cs
@@ -0,0 +1,188 @@
+﻿//int n = 5;
+
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("Cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("tekdir");
+//}
+
+
+
+
+//int m = 9;
+
+//for(int i=1; i<m; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//int m = 6;
+
+//int sum = 0;
+
+//for(int i=1; i<m; i++)
+//{
+//    sum += i; 
+//} Console.WriteLine(sum);
+
+
+
+
+//int m = 7;
+
+//int count = 0;
+
+//for(int i=1; i<m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+//} Console.WriteLine(count);
+
+
+//int n = 1;
+//int m = 8;
+
+//int sum = 0;
+
+//for(int i=n; i<m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+//} Console.WriteLine(sum);
+
+
+
+
+//int n = 5;
+
+//int result = 1;
+
+//for(int i=1; i<=n; i++)
+//{
+//    result *= i;
+
+//} Console.WriteLine(result);
+
+
+
+
+//int[] nums = { 1, 3, 4, 5, 6, 8 };
+//int result = 1;
+
+//for(int i=0; i<nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//    {
+//        result *= nums[i];
+//    }
+//} Console.WriteLine(result);
+
+
+
+
+//int n = 9;
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("Bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("Bolunmur");
+//}
+
+
+
+
+//int n = 8;
+
+//int m = 12;
+
+//int sum = 0;
+
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    sum = n + m;
+//    Console.WriteLine(sum);
+//} else
+//{
+//    Console.WriteLine("odd number is found");
+//}
+
+
+
+
+//int n = 2;
+//int m = 6;
+//int sum = 0;
+
+//for(int i=n; i<m; i++)
+//{
+//    if(i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+//} Console.WriteLine(sum);
+
+
+
+
+//int[] nums = { 3, 7, 6, 2 };
+//int sum = 0;
+
+//for(int i=0; i<nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 1)
+//    {
+//        sum += nums[i];
+//    }
+//} Console.WriteLine(sum);
+
+
+
+
+
+//int[] nums = { 2, 4, 5, 7, 8 };
+//int count = 0;
+
+//for(int i=0; i<nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//    {
+//        count++;
+//    }
+//} Console.WriteLine(count);
+
+
+
+
+//int n = 5;
+//int count = 0;
+
+
+//for(int i=1; i<=n; i++)
+//{
+//    if(n % i == 0)
+//    {
+//        count++;
+//    }
+    
+//}
+//if (count == 2)
+//{
+//    Console.WriteLine("given number is prime");
+//}
+
+//else
+//{
+//    Console.WriteLine("given number is not prime");
+//}
